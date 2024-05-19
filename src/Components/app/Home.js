35f56bc972e2f1e83978bf26b1f97f2a1b274fe0
UJeongff <105771793+UJeongff@users.noreply.github.com>
2024-05-19 23:04:19 +0900

    first commit

---
+++ src/Components/app/Home.js
@@ -0,0 +1,25 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// function Home() {
+//     return (
+//         <div className="container mt-5">
+//             <div className="jumbotron">
+//                 <h1 className="display-4">home</h1>
+//                 <p className="lead">
+//                     pay1oad homepage
+//                 </p>
+//                 <hr className="my-4" />
+//                 <p>
+//                     avocado
+//                 </p>
+// 				<Link to="/bbslist">
+// 					<button className="btn btn-primary btn-lg">Board</button>
+// 				</Link>
+// 				<br></br><br></br>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Home;
